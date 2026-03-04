3f1fe6ed08f118350861c2c76a0e6b3cd5e39ca4
gportodev <gportodev@gmail.com>
2026-03-04 16:59:06 -0300

    feat: add interceptor

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native';
+import { useAuthContext } from '@/context/auth.context';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 function Home() {
+  const { handleLogout } = useAuthContext();
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text>HomeScreen!</Text>
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Sair</Text>
+      </TouchableOpacity>
     </View>
   );
 }
